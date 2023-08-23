7cd33da8edbe21ac9eefd63f9fe90d84597e645b
Vilmor Somera <vsomera77@outlook.com>
2023-08-23 14:53:00 -0700

    user schema

---
+++ backend/models/userModel.ts
@@ -0,0 +1,28 @@
+import { Schema } from "mongoose"
+import mongoose from "mongoose"
+
+interface userSchema {
+    username: string
+    email: string
+    password: string | number
+    createdAt: Date
+}
+
+const userSchema = new Schema<userSchema>({
+    username: {
+        type: String,
+        required: [true, "Please Enter a Name"]
+    },
+    email: {
+        type: String,
+        required: [true, "Please Enter an Email"]
+    },
+    password: {
+        type: String,
+        required: [true, "Please Enter a Password"]
+    }
+}, { timestamps: true })
+
+const collection = "users"
+export default mongoose.model("User", userSchema, collection)
+
