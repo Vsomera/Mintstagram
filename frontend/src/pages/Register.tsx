b38a95e124736c61c0b0f2a0c205ea67415b7ec6
Vilmor Somera <vsomera77@outlook.com>
2023-09-25 19:01:49 -0700

    initial registration page

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,7 +1,36 @@
 const Register = () => {
     return (
         <>
-            Register Page
+            <div className="registration">
+                <div className="register-container">
+                    
+                    <div className="title">
+                        <h1>Registration</h1>
+                    </div>
+
+                    <div className="auth-container">
+                        <div className="auth-input">
+                            <input type="text" required />
+                            <span>U s e r n a m e</span>
+                        </div>
+
+                        <div className="auth-input">
+                            <input type="text" required />
+                            <span>E m a i l</span>
+                        </div>
+
+                        <div className="auth-input" >
+                            <input type="password" required />
+                            <span>P a s s w o r d</span>
+                        </div>
+                        <div className="auth-input" >
+                            <input type="password" required />
+                            <span>C O N F I R M - P a s s w o r d</span>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </>
     )
 }
